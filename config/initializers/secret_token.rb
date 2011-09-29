74502648d66a76b64b202d672421c7f2dc296ec2
Jen-Mei Wu <jenmei@colorxp.com>
2011-09-29 12:03:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyGoLuckyFarm::Application.config.secret_token = 'cbc56d5cb7241c1d8055bff3496cc1712686ac02c5f953f6af6811b0e2dbb83e8cc529bd69215bab6ac2a8693650c805529a0dc5113a070161e6705da068a4b9'
